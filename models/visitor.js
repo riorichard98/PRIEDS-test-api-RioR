3f1d72226ecc5ff0e3c23cdc6b698ff1c3f44ad4
riorichard98 <riorichard12@gmail.com>
2022-07-22 20:44:37 +0700

    setup for seeder visitors

--- models/visitor.js
+++ models/visitor.js
@@ -1,2 +1,30 @@
 const { ObjectId } = require("mongodb");
 const { getDataBase } = require("../config/mongoDb");
+
+class Visitor{
+    static async findAllVisitor(page){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async createVisitor(data){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async findVisitorById(id){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
+
+module.exports = { Visitor }
